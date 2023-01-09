d4d2bddf9c5f83dc548ff5241d15f3fc9871e9e4
anjali9774 <anjalicjm06@gmail.com>
2023-01-09 17:27:14 +0530

    Counter App using JS
    JS is used

---
+++ Counter/index.js
@@ -0,0 +1,11 @@
+let count = 0;
+
+document.getElementById("decrease").onclick = function(){
+    count-=1;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+document.getElementById("increase").onclick = function(){
+    count+=1;
+    document.getElementById("countLabel").innerHTML = count;
+}
